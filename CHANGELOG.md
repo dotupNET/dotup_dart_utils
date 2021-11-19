ff2a2db6244ac87d293b610e61e139187031820e
Peter Ullrich <dotup.software@gmail.com>
2021-11-19 11:40:18 +0100

    typedefs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,11 @@
 - New: typedef void ValueSetter<T>(T value);
 - New: typedef FutureOr<void> FutureValueSetter<T>(T value);
 - New: typedef FutureOr<void> FutureOrValueSetter<T>(T value);
+
+## 2.0.0
+
+- Changed: typedef Map<String, dynamic> ToMap<T>(T item);
+- Changed: typedef T FromMap<T>(Map<String, dynamic> map);
+
+- New: typedef List<Map<String, dynamic>> ToMapList<T>(T item);
+- New: typedef List<T> FromMapList<T>(Map<String, dynamic> map);
--- lib/src/types/TypeDefs.dart
+++ lib/src/types/TypeDefs.dart
@@ -12,5 +12,8 @@ typedef FutureOr<void> FutureOrValueSetter<T>(T value);
 typedef FutureOr<T> FutureValueGetter<T>();
 typedef FutureOr<T> FutureOrValueGetter<T>();
 
-typedef Map<String, Object?> ToMap<T>(T item);
-typedef T FromMap<T>(Map<String, Object?> map);
+typedef Map<String, dynamic> ToMap<T>(T item);
+typedef T FromMap<T>(Map<String, dynamic> map);
+
+typedef List<Map<String, dynamic>> ToMapList<T>(T item);
+typedef List<T> FromMapList<T>(Map<String, dynamic> map);
